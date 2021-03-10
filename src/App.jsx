a6f1d61e8cf6343ad5ba0abaf9300ff9378a545d
Jonathan Girard <jonathan.girard@celliersintelligents.com>
2021-03-09 21:39:44 -0500

    Added 3d particles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import View from "./components/View";
 import FPSStats from "react-fps-stats";
 import { startAcquisition, stopAcquisition } from "./actions";
 import { execute } from "./services/webgl";
+import ParticleField from "react-particles-webgl";
 
 var timer;
 
@@ -13,9 +14,11 @@ const App = (props) => {
     { id: "canvas1", width: 800, height: 200 },
     { id: "canvas2", width: 800, height: 200 },
   ];
+  const [isParticlesEnabled, setIsParticlesEnabled] = useState(false);
 
   const onStartAcquisition = () => (timer = setInterval(execute, 13));
   const onStopAcquisition = () => clearInterval(timer);
+  const onParticlesClick = () => setIsParticlesEnabled(!isParticlesEnabled);
 
   return (
     <div>
@@ -26,6 +29,9 @@ const App = (props) => {
           </button>
           <button type="button" onClick={onStopAcquisition}>
             Stop
+          </button>
+          <button type="button" onClick={onParticlesClick}>
+            {!isParticlesEnabled ? "Add 3D" : "Remove 3D"}
           </button>
         </div>
         <FPSStats />
@@ -38,6 +44,11 @@ const App = (props) => {
           height={view.height}
         />
       ))}
+      {isParticlesEnabled && (
+        <div className="put-behind fullscreen">
+          <ParticleField />
+        </div>
+      )}
     </div>
   );
 };
